360bae3bbe3516e4857aa3c7d4823296faefd257
Nick Freear <nfreear@yahoo.co.uk>
2011-04-10 21:25:26 +0100

    Adding Moodle 2 language pack - lang/en/

---
+++ lang/en/filter_scratchembed.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Strings for component 'filter_scratchembed', language 'en'.
+ *
+ * @package   filter_scratchembed
+ * @copyright (c) 2010 Nicholas Freear {@link http://freear.org.uk}.
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v2 or later
+ */
+$string['filtername'] = 'Scratch embed';
+
+$string['attrib'] = 'Explore the Scratch project by {$a}';
+$string['rights'] = 'Some rights reserved';
+$string['nojava'] = 'Your browser needs Java enabled to view Scratch projects.';
