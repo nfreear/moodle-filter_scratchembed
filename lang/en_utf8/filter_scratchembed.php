6fbcce9241047b8961f237606b6866bceb2ca2b1
Nick Freear <nfreear@yahoo.co.uk>
2011-04-10 21:25:50 +0100

    Adding Moodle 1.9 language pack - lang/en_utf8/

---
+++ lang/en_utf8/filter_scratchembed.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Strings for component 'filter_scratchembed', language 'en'.
+ *
+ * @package   filter_scratchembed
+ * @copyright (c) 2010 Nicholas Freear {@link http://freear.org.uk}.
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v2 or later
+ *
+ * Moodle 1.9.x compatible.
+ */
+global $CFG;
+require_once $CFG->dirroot.'/filter/scratchembed/lang/en/filter_scratchembed.php';
